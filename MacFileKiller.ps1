985b6982cab8fb5e70b1771c716d68b99749a63c
tomtiv <tomtiv@gmail.com>
2016-03-06 23:13:55 -0500

    adding file

---
+++ MacFileKiller.ps1
@@ -0,0 +1,35 @@
+﻿cls
+
+"--------------------------"
+" Starting Mac File Killer "
+"--------------------------"
+" "
+
+#$LocalDrives = gwmi Win32_LogicalDisk -filter "DriveType=3"
+$NetworkDrives = gwmi Win32_LogicalDisk -filter "DriveType=4"
+
+#"Found " + $LocalDrives.Count + " Local Drive(s)"
+"Found " + $NetworkDrives.Count + " Network Drive(s)"
+
+#foreach ($disk in Get-WMIObject Win32_LogicalDisk | Where-Object { $_.DriveType -eq 3 -or $_.DriveType -eq 4})
+foreach ($disk in $NetworkDrives)
+{
+	" "
+#	if($disk.DeviceID -eq "X:")
+#	{
+#		"Skipping " + $disk.DeviceID
+#	}
+#	else
+#	{
+		"--------------------------"
+		"Processing " + $disk.DeviceID
+		cd -LiteralPath $disk.DeviceID
+		dir * -include "._*", ".DS_*" -Recurse -Force | Remove-Item -Force
+		"Done With " + $disk.DeviceID
+		"--------------------------"		
+#	}
+	" "
+}
+"--------------------------"
+"ALL DONE!"
+"--------------------------"
